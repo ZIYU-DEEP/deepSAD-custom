241f8ec73e138477cb7174cd57d6caf22705ae3f
Ziyu Ye <ziyuye@uchicago.edu>
2020-02-19 18:24:18 -0600

    Update campus_drive.sh

--- campus_drive.sh
+++ campus_drive.sh
@@ -1,6 +1,5 @@
-python create_arrays.py normal campus_drive campus_drive_sigOver_5ms
-python create_arrays.py normal campus_drive campus_drive_sigOver_20ms
-python create_arrays.py normal campus_drive campus_drive_LOS-5M-USRP2
-python create_arrays.py normal campus_drive campus_drive_LOS-5M-USRP3
-python create_arrays.py normal campus_drive campus_drive_NLOS-5M-USRP1
-python create_arrays.py normal campus_drive campus_drive_Dynamics-5M-USRP1
+python create_squeeze_array.py abnormal campus_drive_LOS-5M-USRP1 1086
+python create_squeeze_array.py abnormal campus_drive_LOS-5M-USRP2 1086
+python create_squeeze_array.py abnormal campus_drive_LOS-5M-USRP3 1086
+python create_squeeze_array.py abnormal campus_drive_NLOS-5M-USRP1 1086
+python create_squeeze_array.py abnormal campus_drive_Dynamics-5M-USRP1 1086
