f2090db7708c9932bcc7d4270688f4d4de5dabb2
Ziyu Ye <ziyuye@uchicago.edu>
2020-03-08 19:23:20 -0500

    Update lstm.py

--- src/networks/lstm.py
+++ src/networks/lstm.py
@@ -12,14 +12,10 @@ class LSTM_Net(BaseNet):
 
         self.rep_dim = rep_dim
         self.lstm1 = nn.GRU(128, 128, batch_first=True)
-#         self.lstm2 = nn.GRU(64, 32, batch_first=True)
-#         self.lstm3 = nn.GRU(32, 16, batch_first=True)
         self.fc1 = nn.Linear(128 * 1000, self.rep_dim, bias=False)
 
     def forward(self, x):
         x, _ = self.lstm1(x)
-#         x, _ = self.lstm2(x)
-#         x, _ = self.lstm3(x)
         x = x.reshape(x.size(0), 128 * 1000).contiguous()
         x = self.fc1(x)
         return x
@@ -34,16 +30,12 @@ class LSTM_Decoder(BaseNet):
 
         # Decoder network
         self.lstm1 = nn.GRU(128, 128, batch_first=True)
-#         self.lstm2 = nn.GRU(32, 64, batch_first=True)
-#         self.lstm3 = nn.GRU(64, 128, batch_first=True)
         self.fc1 = nn.Linear(self.rep_dim, 128 * 1000)
 
     def forward(self, x):
         x = self.fc1(x)
         x = x.view(x.size(0), 1000, 128).contiguous()
         x, _ = self.lstm1(x)
-#         x, _ = self.lstm2(x)
-#         x, _ = self.lstm3(x)
         x = torch.sigmoid(x)
         return x
 
